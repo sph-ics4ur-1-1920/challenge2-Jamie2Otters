1fec9d40e8bc46bd6973930a7b7902ddb5a1e28b
Jamie2Otters <32065389+Jamie2Otters@users.noreply.github.com>
2019-10-14 16:03:04 -0400

    Bonus challenge left and I'm done

--- Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -19,7 +19,10 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        }else if (transform.position.z < lowerBound){
+        }else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Gameover!");
             Destroy(gameObject);
-        }}
+        }
+    }
 }
